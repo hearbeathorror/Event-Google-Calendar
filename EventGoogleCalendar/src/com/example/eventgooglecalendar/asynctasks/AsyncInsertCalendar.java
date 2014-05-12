7c9e9834467f711b0c93b01834b45ab9450af7da
dhara <hearbeat.horror@gmail.com>
2014-05-12 16:19:02 +0530

    initial commit

---
+++ EventGoogleCalendar/src/com/example/eventgooglecalendar/asynctasks/AsyncInsertCalendar.java
@@ -0,0 +1,42 @@
+package com.example.eventgooglecalendar.asynctasks;
+
+/*
+ * Copyright (c) 2012 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import com.example.eventgooglecalendar.CalendarSampleActivity;
+import com.example.eventgooglecalendar.models.CalendarInfo;
+import com.google.api.services.calendar.model.Calendar;
+
+import java.io.IOException;
+
+/**
+ * Asynchronously insert a new calendar.
+ * 
+ * @author Yaniv Inbar
+ */
+public class AsyncInsertCalendar extends CalendarAsyncTask {
+
+  private final Calendar entry;
+
+  public AsyncInsertCalendar(CalendarSampleActivity calendarSample, Calendar entry) {
+    super(calendarSample);
+    this.entry = entry;
+  }
+
+  @Override
+  protected void doInBackground() throws IOException {
+    Calendar calendar = client.calendars().insert(entry).setFields(CalendarInfo.FIELDS).execute();
+    model.add(calendar);
+  }
+}
